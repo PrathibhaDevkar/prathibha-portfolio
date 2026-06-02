f4e926bac8af910b5730b72ac158e6b9b894641b
Prathibha Devkar <43299959+PrathibhaDevkar@users.noreply.github.com>
2026-06-02 16:23:54 -0500

    Update contact.ts

--- data/contact.ts
+++ data/contact.ts
@@ -3,5 +3,5 @@ export const contactLinks = {
   linkedin: "https://linkedin.com/in/prathibha-devkar-3096",
   github: "https://github.com/PrathibhaDevkar",
   location: "Dallas, TX",
-  resumeLink: "/Prathibha_Devkar_Resume.pdf"
+  resumeLink: "/RPD_SWE.pdf"
 };
